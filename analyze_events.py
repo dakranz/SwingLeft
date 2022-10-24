935399f0f2c058c8ce08dbb1605ec27844fd0314
David Kranz <david.a.kranz@gmail.com>
2022-10-24 16:09:54 -0400

    Make analyze_events able to run unattended, and actually delete orphaned and duplicate events.

--- analyze_events.py
+++ analyze_events.py
@@ -1,8 +1,11 @@
+import argparse
 import datetime
 import sys
 import time
+import traceback
 
 import events
+import slack
 import the_events_calendar
 
 skip_list = {'news-magic': [],
@@ -17,25 +20,35 @@ def canonicalize_url(url):
     return 'https://www.mobilize.us/swingbluealliance/event/{}/'.format(mobilize_id)
 
 
-def find_duplicate_calendar_events():
-    all_events = events.get_calendar_events()
+def find_duplicate_calendar_events(all_events):
     url_map = {}
     for event in all_events:
         for url in events.get_urls(event['description']):
             url = canonicalize_url(url)
             candidate = (url, event['start_date'])
             if candidate in url_map:
-                url_map[candidate].add(event['url'])
+                url_map[candidate].append(event)
             else:
-                url_map[candidate] = set([event['url']])
-    for (url, start), event_urls in url_map.items():
-        if len(event_urls) == 1:
+                url_map[candidate] = [event]
+    for (url, start), event_list in url_map.items():
+        # dedup
+        event_set = []
+        for event in event_list:
+            if event not in event_set:
+                event_set.append(event)
+        if len(event_set) == 1:
             continue
         if url in skip_list[the_events_calendar.calendar_name]:
             continue
-        print(url)
-        for event_url in event_urls:
-            print(event_url)
+        last_mod = max(event['modified'] for event in event_set)
+        for event in event_set:
+            if event['modified'] == last_mod:
+                print('Keeping: ', event['url'])
+            else:
+                print('Deleting duplicate: ', event['url'])
+                if not args.dry_run:
+                    events.delete_calendar_event(event['id'])
+                    time.sleep(1)
         print('\n')
 
 
@@ -43,10 +56,8 @@ def time_slot_string(event_id, start):
     return '{}#{}'.format(event_id, start)
 
 
-def find_orphaned_calendar_events(org):
+def find_orphaned_calendar_events(org, calendar_events, mobilize_events):
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    calendar_events = events.get_calendar_events()
-    mobilize_events = events.get_all_mobilize_events()
     all_slots = set()
     for event in mobilize_events:
         for slot in event['timeslots']:
@@ -56,19 +67,44 @@ def find_orphaned_calendar_events(org):
         if event['start_date'] <= now or 'mobilize.us/' + org not in event['website']:
             continue
         if time_slot_string(events.get_event_id(event['website']), event['start_date']) not in all_slots:
-            print(event['url'])
-            #events.delete_calendar_event(event['id'])
-            time.sleep(1)
+            print('Deleting orphaned: ', event['url'])
+            if not args.dry_run:
+                events.delete_calendar_event(event['id'])
+                time.sleep(1)
+    print('\n')
+
+
+def report_error(message):
+    header = "<!channel> *Duplicate and orphan detection failed*"
+    blocks = [{"type": "section", "text": {"type": "mrkdwn", "text": header}},
+              {"type": "section", "text": {"type": "plain_text", "text": message}}]
+    slack.post_message('automation', text=header, blocks=blocks)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-y", "--dry_run", action="store_true", help="Read events but do not update the sheet.")
+parser.add_argument("-r", "--report", action="store_true", help="Report errors to slack.")
+parser.add_argument("calendar", help="Which calendar to analyze.")
+args = parser.parse_args()
 
 
 def main():
-    the_events_calendar.set_global_calendar(sys.argv[1])
-    if the_events_calendar.calendar_name == 'sba':
-        org = 'swingbluealliance'
-    else:
-        org = 'news-magic'
-    find_duplicate_calendar_events()
-    #find_orphaned_calendar_events(org)
+    try:
+        the_events_calendar.set_global_calendar(args.calendar)
+        if the_events_calendar.calendar_name == 'sba':
+            org = 'swingbluealliance'
+        else:
+            org = 'news-magic'
+        calendar_events = events.get_calendar_events()
+        mobilize_events = events.get_all_mobilize_events()
+        find_duplicate_calendar_events(calendar_events)
+        find_orphaned_calendar_events(org, calendar_events, mobilize_events)
+    except Exception as e:
+        err_message = traceback.format_exc()
+        if args.report:
+            report_error(err_message)
+        else:
+            print(err_message)
 
 
 main()
